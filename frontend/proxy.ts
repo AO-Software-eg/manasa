98b196333938350110238e57294665a950ebffe9
som3a-dev <omar.eltayeb54@gmail.com>
2026-04-19 01:28:39 +0200

    Update proxy.ts

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -4,7 +4,8 @@ import type { NextRequest } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export default function proxy(request: NextRequest) {
   const url = request.nextUrl.clone();
-  return NextResponse.redirect(url);
+  console.log(url);
+  return NextResponse.rewrite(url);
 }
 
 export const config = {};
