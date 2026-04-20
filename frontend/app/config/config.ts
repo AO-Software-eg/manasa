d560a4446d2b8d67a6164a36def1d842b9c88287
som3a-dev <omar.eltayeb54@gmail.com>
2026-04-20 23:40:28 +0200

    update api config with dev & prod checks

--- frontend/app/config/config.ts
+++ frontend/app/config/config.ts
@@ -1,2 +1,13 @@
-export const API_URL: string =
-  process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:5000';
+let api_url: string | undefined;
+
+if (process.env.NODE_ENV == 'development') {
+  api_url = process.env.NEXT_PUBLIC_LOCAL_API_URL;
+} else if (process.env.NODE_ENV == 'production') {
+  api_url = process.env.NEXT_PUBLIC_API_URL;
+}
+
+if (!api_url) {
+  throw new Error('Backend API url not found in environment variables.');
+}
+
+export const API_URL: string = api_url;
