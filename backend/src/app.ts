c8c31c58c1d185e260055e6156ae2df93a5b1752
som3a-dev <omar.eltayeb54@gmail.com>
2026-04-24 15:59:26 +0300

    course route setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -160,6 +160,19 @@ app
     res.status(200).send();
   });
 
+app.route('/course/:courseId').get(async (req: Request, res: Response) => {
+  const courseId = req.params.courseId;
+
+  if (typeof courseId !== 'string') {
+    return res.status(401).json({ message: 'Invalid course ID paramater' });
+  }
+  if (/^\d+$/.test(courseId) === false) {
+    return res.status(401).json({ message: 'Invalid course ID paramater' });
+  }
+
+  return res.status(200).json('Yay response');
+});
+
 app.route('/video/:videoId').get(async (req: Request, res: Response) => {
   if (!req.cookies.user_token) {
     return res.status(401).json({ message: 'Unauthorized' });
