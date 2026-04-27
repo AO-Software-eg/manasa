e0c2ee1d5bda87994781ec61273028c89532e793
abdelrahman mohamed <97918409+abdelrahman1mohamed1@users.noreply.github.com>
2026-04-27 15:20:13 +0300

    error handle

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,17 @@
 import app from './app.ts';
 import config from './config.ts';
 
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+
+
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
 });
