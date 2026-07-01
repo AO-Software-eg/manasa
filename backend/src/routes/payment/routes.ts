d3ccf67ac04709f098fef01cad5ec877f53330ad
abdelrahman mohamed <97918409+abdelrahman1mohamed1@users.noreply.github.com>
2026-07-01 23:52:04 +0300

    remove unused nprogress import trace from global.css

--- backend/src/routes/payment/routes.ts
+++ backend/src/routes/payment/routes.ts
@@ -127,7 +127,7 @@ router
         special_reference: `${payment.id}-${payment.createdAt}`,
         expiration: 3600,
         notification_url: `${process.env.NGROK_BASE_URL}/payment/paymob-callback`,
-        redirection_url: `https://manasa-deploy-main-git-paymob-abdelrahman1devs-projects.vercel.app/payment/result?courseId=${buyData.itemId}`,
+        redirection_url: `${process.env.FRONTEND_LOCAL_URL}/payment/result?courseId=${buyData.itemId}`,
       });
 
       const headers: HeadersInit = new Headers();
