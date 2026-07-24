ab91321da83e7303049f9a8eb5dd1920f9120a95
som3a-dev <omar.eltayeb54@gmail.com>
2026-07-24 06:10:06 +0300

    Update auth.routes.ts

--- backend/src/routes/auth/auth.routes.ts
+++ backend/src/routes/auth/auth.routes.ts
@@ -8,7 +8,12 @@ const router = express.Router();
 router.route('/signup').post(bodyParser.json(), controller.signup);
 router.route('/login').post(bodyParser.json(), controller.login);
 router.route('/logout').post(bodyParser.json(), controller.logout);
-router.route('/reset-password').post(bodyParser.json(), controller.resetPassword);
+router
+  .route('/reset-password')
+  .post(bodyParser.json(), controller.resetPassword);
+router
+  .route('/reset-password/token')
+  .post(bodyParser.json(), controller.resetPasswordToken);
 
 router.route('/akedly/send').post(bodyParser.json(), controller.akedlySend);
 router.route('/akedly/challenge').get(controller.akedlyChallenge);
